09abe4689d6a27965ab77590a98f693a63fda4dd
mihaj <miha.jakovac@gmail.com>
2020-10-23 17:58:15 +0200

    Initial commit

---
+++ test/QAToolKit.Source.Swagger.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace QAToolKit.Source.Swagger.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
