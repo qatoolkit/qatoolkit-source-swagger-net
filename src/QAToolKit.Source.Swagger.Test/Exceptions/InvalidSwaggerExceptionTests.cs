6b8ecd178ab3883a12e7b758688691687204b1cb
mihaj <miha.jakovac@gmail.com>
2021-11-10 17:05:56 +0100

    new exceptions

---
+++ src/QAToolKit.Source.Swagger.Test/Exceptions/InvalidSwaggerExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using QAToolKit.Source.Swagger.Exceptions;
+using Xunit;
+
+namespace QAToolKit.Source.Swagger.Test.Exceptions
+{
+    public class InvalidSwaggerExceptionTests
+    {
+        [Fact]
+        public void CreateExceptionTest_Successful()
+        {
+            var exception = new InvalidSwaggerException("my error");
+
+            Assert.Equal("my error", exception.Message);
+        }
+
+        [Fact]
+        public void CreateExceptionWithInnerExceptionTest_Successful()
+        {
+            var innerException = new Exception("Inner");
+            var exception = new InvalidSwaggerException("my error", innerException);
+
+            Assert.Equal("my error", exception.Message);
+            Assert.Equal("Inner", innerException.Message);
+        }
+    }
+}
---
+++ src/QAToolKit.Source.Swagger/Exceptions/InvalidSwaggerException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Microsoft.OpenApi.Models;
+
+namespace QAToolKit.Source.Swagger.Exceptions
+{
+    /// <summary>
+    /// InvalidSwagger exception
+    /// </summary>
+    [Serializable]
+    public class InvalidSwaggerException : Exception
+    {
+        /// <summary>
+        /// InvalidSwagger exception
+        /// </summary>
+        public InvalidSwaggerException(string message) : base(message)
+        {
+        }
+
+        /// <summary>s
+        /// InvalidSwagger exception
+        /// </summary>
+        public InvalidSwaggerException(string message, IList<OpenApiError> errors) : base(message += string.Join(",",
+            errors.Select(s => new KeyValuePair<string, string>(s.Message, s.Pointer)).ToList()))
+        {
+        }
+
+        /// <summary>
+        /// InvalidSwagger exception
+        /// </summary>
+        public InvalidSwaggerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// InvalidSwagger exception
+        /// </summary>
+        protected InvalidSwaggerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
