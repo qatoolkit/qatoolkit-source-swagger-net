df52c4f1d51f7727da8042a7d32e79ba87296f60
mihaj <miha.jakovac@gmail.com>
2023-01-14 09:48:18 +0100

    NET7 case

--- src/QAToolKit.Source.Swagger/SwaggerProcessor.cs
+++ src/QAToolKit.Source.Swagger/SwaggerProcessor.cs
@@ -265,7 +265,7 @@ namespace QAToolKit.Source.Swagger
 #elif NETCOREAPP3_1
                 case "patch":
                     return HttpMethod.Patch;
-#elif NET6_0
+#elif NET7_0
                 case "patch":
                     return HttpMethod.Patch;
 #endif
